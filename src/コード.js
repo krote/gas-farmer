8409bf9cd2420b17183787b47d4a65fb6a77235a
krote <krote0924@gmail.com>
2022-09-10 06:58:09 +0900

    say hello

--- src/コード.js
+++ src/コード.js
@@ -1,3 +1,13 @@
 function myFunction() {
   Logger.log('Hello Gas!');
 }
+
+function getAmedas( param_pref_ja, param_address) {
+    var requestUrl = "https://jjwd.info/api/v2/stations/search?pref_ja=" + param_pref_ja;
+    if(param_address != null && param_address != ""){
+      requestUrl += "&address=" + param_address;
+    }
+    var response = UrlFetchApp.fetch(requestUrl);
+    var res = JSON.parse(response.getContentText());
+  
+  }
