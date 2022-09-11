d78e61fbfa14c80826498a1bd029cd1d7ffd5bed
krote <krote0924@gmail.com>
2022-09-11 17:07:46 +0900

    change data source

--- src/コード.js
+++ src/コード.js
@@ -7,7 +7,7 @@ function doGet() {
 }
 
 function getSheetData() {
-    const sh = ss.getSheetByName("設定");
+    const sh = ss.getSheetByName("品種");
     const response = JSON.stringify(sh.getDataRange().getValues());
     return response;
 }
